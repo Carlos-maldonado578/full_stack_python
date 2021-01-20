e614529ac9bc9a9b35e41a94ac2964b755548273
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-01-20 19:48:26 -0300

    actividad random

--- python/fundamentals/funciones_intermedias_I.py
+++ python/fundamentals/funciones_intermedias_I.py
@@ -6,3 +6,9 @@ print(randInt()) 		# should print a random integer between 0 to 100
 print(randInt(max=50)) 	# should print a random integer between 0 to 50
 print(randInt(min=50)) 	# should print a random integer between 50 to 100
 print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+
+#Respuestas
+# 0.036284680992570095
+# 0.4764200473648649
+# 0.6317589725816279
+# 0.8701418910466693
